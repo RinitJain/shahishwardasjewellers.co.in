859acc30282f5079f9c9081dc8c6c1535b6257d7
Rinit Jain <rinitjain9@gmail.com>
2025-06-22 06:38:17 +0000

    Not able to publish my app through the publish button you have given

--- src/lib/firebaseConfig.ts
+++ src/lib/firebaseConfig.ts
@@ -8,7 +8,7 @@ export const firebaseConfig = {
   apiKey: "AIzaSyAPSL-JUi8e8ehyrhOH8QIz6yIzfWq6Lew",
   authDomain: "sij-website-e2d0c.firebaseapp.com",
   projectId: "sij-website-e2d0c",
-  storageBucket: "sij-website-e2d0c.firebasestorage.app", // Corrected from .app to .appspot.com if it was a typo, Firebase usually uses .appspot.com for storage. If .firebasestorage.app is correct, I'll keep it. The provided config uses .app.
+  storageBucket: "sij-website-e2d0c.appspot.com",
   messagingSenderId: "700450565463",
   appId: "1:700450565463:web:1ac2ce683af9581b1fa212",
   measurementId: "G-XLGDV4GJQ2"
